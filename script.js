68c162ebd222465faa8f8d6f298b721b83f92b78
Vivek Singh <rogerthatvivek@gmail.com>
2022-10-24 15:07:11 +0530

    Add and Remove from Cart Functionality

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ const products=document.getElementById('products')
 const qty=document.querySelector('.cart-number')
 const toast=document.getElementById('toast')
 const cart=document.getElementById('cart')
+const seeCart=document.getElementById('open-cart')
 const closeCart=document.getElementById('close')
+const items=document.querySelectorAll('.item')
+const total=document.getElementById('total')
+const remove=document.getElementById('items')
+console.log(items)
 
+remove.addEventListener('click', removeItem)
+
+seeCart.addEventListener('click', showCart)
 cart.addEventListener('click', showCart)
 products.addEventListener('click', addToCart)
 closeCart.addEventListener('click', hideCart)
 
-let count=0
+let count=0, cartTotal=0.00
 
 function showCart(){
     document.getElementById('popup-container').style.display="flex"
@@ -27,29 +35,78 @@ function addToCart(e){
     switch(e.target.id){
         case '1':   {
             createNotification('Album 1')
+            items[0].style.display="flex"
+            cartTotal+=549.99
         }
         break;
         case '2':   {
             createNotification('Album 2')
+            items[1].style.display="flex"
+            cartTotal+=749.99
         }
         break;
         case '3':   {
             createNotification('Album 3')
+            items[2].style.display="flex"
+            cartTotal+=439.99
         }
         break;
         case '4':   {
             createNotification('Album 4')
+            items[3].style.display="flex"
+            cartTotal+=849.99
         }
         break;
         case '5':   {
             createNotification('T-Shirt')
+            items[4].style.display="flex"
+            cartTotal+=499.99
         }
         break;
         case '6':   {
             createNotification('Coffee Cup')
+            items[5].style.display="flex"
+            cartTotal+=349.99
+        }
+        break;
+    }    
+    total.innerHTML=`₹ ${parseFloat(cartTotal).toFixed(2)}`
+}
+
+function removeItem(e){
+    switch(e.target.id){
+        case '1':   {
+            items[0].style.display="none"
+            cartTotal-=549.99
+        }
+        break;
+        case '2':   {
+            items[1].style.display="none"
+            cartTotal-=749.99
+        }
+        break;
+        case '3':   {
+            items[2].style.display="none"
+            cartTotal-=439.99
+        }
+        break;
+        case '4':   {
+            items[3].style.display="none"
+            cartTotal-=849.99
+        }
+        break;
+        case '5':   {
+            items[4].style.display="none"
+            cartTotal-=499.99
+        }
+        break;
+        case '6':   {
+            items[5].style.display="none"
+            cartTotal-=349.99
         }
         break;
     }
+    total.innerHTML=`₹ ${parseFloat(cartTotal).toFixed(2)}`
 }
 
 function createNotification(prod){
